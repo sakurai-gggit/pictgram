a930c60377a1fe0e7e9c775774aeb48b81d331fc
sakurai <mene10morim@gmail.com>
2026-02-01 17:00:26 +0900

    コメント画面の作成

---
+++ src/main/java/com/example/pictgram/form/CommentForm.java
@@ -0,0 +1,18 @@
+package com.example.pictgram.form;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class CommentForm {
+
+    private Long id;
+
+    private Long topicId;
+
+    @NotEmpty
+    @Size(max = 1000)
+    private String description;
+}

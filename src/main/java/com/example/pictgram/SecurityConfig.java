2fbb43fd9e271746e0a372de23d575184a015e6c
sakurai <mene10morim@gmail.com>
2026-02-11 14:05:15 +0900

    ソーシャルログイン機能の追加

--- src/main/java/com/example/pictgram/SecurityConfig.java
+++ src/main/java/com/example/pictgram/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.pictgram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -16,6 +27,9 @@ import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import com.example.pictgram.entity.SocialUser;
+import com.example.pictgram.entity.User;
+import com.example.pictgram.entity.User.Authority;
 import com.example.pictgram.filter.FormAuthenticationProvider;
 import com.example.pictgram.repository.UserRepository;
 
@@ -27,6 +41,7 @@ public class SecurityConfig {
 	private UserRepository repository;
 	@Autowired
 	private FormAuthenticationProvider authenticationProvider;
+	protected static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector)
@@ -58,6 +73,14 @@ public class SecurityConfig {
 						.defaultSuccessUrl("/topics") // ログイン成功時の遷移先
 						.failureUrl("/login-failure") // ログイン失敗時の遷移先
 						.permitAll()) // 未ログインでもアクセス可能
+				.oauth2Login(oauth2 -> oauth2
+						.loginPage("/login") // ログイン画面
+						.defaultSuccessUrl("/topics") // ログイン成功時の遷移先
+						.failureUrl("/login-failure") // ログイン失敗時の遷移先
+						.permitAll() // 未ログインでもアクセス可能
+						.userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+								.oidcUserService(this.oidcUserService())
+								.userService(this.oauth2UserService())))
 				.logout(logout -> logout
 						.logoutSuccessUrl("/logout-complete") // ログアウト成功時の遷移先
 						.invalidateHttpSession(true)
@@ -88,4 +111,47 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+		final OidcUserService delegate = new OidcUserService();
+		return (userRequest) -> {
+			OidcUser oidcUser = delegate.loadUser(userRequest);
+			OAuth2AccessToken accessToken = userRequest.getAccessToken();
+
+			log.debug("accessToken={}", accessToken);
+
+			oidcUser = new DefaultOidcUser(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUser.getUserInfo());
+			String email = oidcUser.getEmail();
+			User user = repository.findByUsername(email);
+			if (user == null) {
+				user = new User(email, oidcUser.getFullName(), "", Authority.ROLE_USER);
+				repository.saveAndFlush(user);
+			}
+			oidcUser = new SocialUser(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUser.getUserInfo(),
+					user.getUserId());
+
+			return oidcUser;
+		};
+	}
+
+	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+		DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+		return request -> {
+			OAuth2User oauth2User = delegate.loadUser(request);
+
+			log.debug(oauth2User.toString());
+
+			String name = oauth2User.getAttribute("login");
+			User user = repository.findByUsername(name);
+			if (user == null) {
+				user = new User(name, name, "", Authority.ROLE_USER);
+				repository.saveAndFlush(user);
+			}
+			SocialUser socialUser = new SocialUser(oauth2User.getAuthorities(), oauth2User.getAttributes(), "id",
+					user.getUserId());
+
+			return socialUser;
+		};
+	}
+
 }
